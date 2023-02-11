73c5764556680d14e88d7d0e1dcbb66c2684e93e
Andrej Kovacevic <glazba@gmail.com>
2023-02-11 18:47:14 +0100

    Timer Fill

--- GDTV - 2D - Quiz Master/Assets/Scripts/Timer.cs
+++ GDTV - 2D - Quiz Master/Assets/Scripts/Timer.cs
@@ -1,13 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour {
 
     [SerializeField] private float timeToCompleteQuestion = 30f;
     [SerializeField] private float timeToShowCorrectAnswer = 10f;
 
-    public bool isAnsweringQuestion = false;
+    [SerializeField] private Image filledImage;
+
+    public bool loadNextQuestion;
+    public bool isAnsweringQuestions = false;
     
     private float timerValue;
 
@@ -15,17 +20,39 @@ public class Timer : MonoBehaviour {
         UpdateTimer();
     }
 
+    public void CancelTimer() {
+        timerValue = 0;
+    }
+
     private void UpdateTimer() {
         timerValue -= Time.deltaTime;
 
-        if (timerValue <= 0 && !isAnsweringQuestion) {
-            isAnsweringQuestion = true;
-            timerValue = timeToShowCorrectAnswer;
-        } else if (timerValue <= 0 && isAnsweringQuestion) {
-            isAnsweringQuestion = false;
-            timerValue = timeToCompleteQuestion;
+        if (isAnsweringQuestions) {
+            if (timerValue > 0) {
+                ImageFill();
+            } else if (timerValue <= 0) {
+                isAnsweringQuestions = false;
+                timerValue = timeToShowCorrectAnswer;
+            }
+        } else if (!isAnsweringQuestions) {
+            if (timerValue > 0) {
+                ImageFill();
+            } else if (timerValue <= 0) {
+                isAnsweringQuestions = true;
+                timerValue = timeToCompleteQuestion;
+                loadNextQuestion = true;
+            }
         }
 
         Debug.Log(timerValue);
+    }
+
+    private void ImageFill() {
+        if (isAnsweringQuestions) {
+            filledImage.fillAmount = timerValue / timeToCompleteQuestion;
+        } else if (!isAnsweringQuestions) {
+            filledImage.fillAmount = 1 - (timerValue / timeToShowCorrectAnswer);
+        }
+        
     }
 }
