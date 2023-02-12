7dd159671a5c6e9b79c893b63d6d5042d7066451
Andrej Kovacevic <glazba@gmail.com>
2023-02-12 01:21:52 +0100

    Animation State in Code

---
+++ GDTV - 2D - TileVania/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private const string IS_RUNNING = "isRunning";
+
+    Vector2 moveInput;
+    Rigidbody2D myRigidbody;
+    [SerializeField] float moveSpeed = 4.0f;
+    [SerializeField] Transform playerVisual;
+    Animator animator;
+    bool playerHasHorizontalSpeed;
+
+    private void Start() {
+        myRigidbody= GetComponent<Rigidbody2D>();
+        animator = playerVisual.GetComponent<Animator>();
+    }
+
+    private void Update() {
+        IsMoving();        
+        Run();
+        UpdateAnimation();
+        FlipSprite();
+    }
+
+    private void IsMoving() {
+        playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+    }
+
+    private void UpdateAnimation() {
+        animator.SetBool(IS_RUNNING, playerHasHorizontalSpeed);        
+    }
+
+    private void FlipSprite() {        
+        if (playerHasHorizontalSpeed) {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
+        }
+    }
+
+    private void Run() {
+        Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
+    }
+
+    void OnMove(InputValue value) {
+        moveInput = value.Get<Vector2>();
+    }
+}
