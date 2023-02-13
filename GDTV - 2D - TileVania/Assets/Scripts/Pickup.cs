9c8c225793fa00365662a76ba36ee073dd723d12
Andrej Kovacevic <glazba@gmail.com>
2023-02-13 22:56:48 +0100

    Coin SFX

--- GDTV - 2D - TileVania/Assets/Scripts/Pickup.cs
+++ GDTV - 2D - TileVania/Assets/Scripts/Pickup.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
+    [SerializeField] private AudioClip coinPickupSFX;
+
+    private void Awake() {
+
+    }
+
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.tag == "Player") {
+            AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
             Destroy(gameObject);
         }        
     }
