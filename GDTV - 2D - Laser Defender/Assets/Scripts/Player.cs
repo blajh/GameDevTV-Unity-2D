900cafc9563516e136f1f40825336bf4c6235324
Andrej Kovacevic <glazba@gmail.com>
2023-02-14 14:45:40 +0100

    Player Viewport Boundaries

--- GDTV - 2D - Laser Defender/Assets/Scripts/Player.cs
+++ GDTV - 2D - Laser Defender/Assets/Scripts/Player.cs
@@ -8,15 +8,34 @@ public class Player : MonoBehaviour
     [SerializeField] private float moveSpeed = 4.0f;
 
     private Vector2 rawInput;
-    private Vector3 movementDelta;
+
+    [SerializeField] private float paddingLeft;
+    [SerializeField] private float paddingRight;
+    [SerializeField] private float paddingTop;
+    [SerializeField] private float paddingBottom;
+    private Vector2 minimumBounds;
+    private Vector2 maximumBounds;
+
+    private void Start() {
+        InitializeBounds();
+    }
 
     private void Update() {
         Move();
     }
 
+    private void InitializeBounds() {
+        Camera mainCamera = Camera.main;
+        minimumBounds = mainCamera.ViewportToWorldPoint(new Vector2(0f, 0f));
+        maximumBounds = mainCamera.ViewportToWorldPoint(new Vector2(1f, 1f));
+    }
+
     private void Move() {
-        movementDelta = rawInput;
-        transform.position += moveSpeed * Time.deltaTime * movementDelta;
+        Vector2 movementDelta = moveSpeed * Time.deltaTime * rawInput;
+        Vector2 newPosition = new Vector2();
+        newPosition.x = Mathf.Clamp(transform.position.x + movementDelta.x, minimumBounds.x + paddingLeft, maximumBounds.x - paddingRight);
+        newPosition.y = Mathf.Clamp(transform.position.y + movementDelta.y, minimumBounds.y + paddingBottom, maximumBounds.y - paddingTop);
+        transform.position = newPosition;
     }
 
     private void OnMove(InputValue value) {
