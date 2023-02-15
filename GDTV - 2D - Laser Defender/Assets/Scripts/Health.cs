beaa188989bc690bb4ded843c0297ee5f4c0a590
Andrej Kovacevic <glazba@gmail.com>
2023-02-15 19:15:35 +0100

    Particle Effects

--- GDTV - 2D - Laser Defender/Assets/Scripts/Health.cs
+++ GDTV - 2D - Laser Defender/Assets/Scripts/Health.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] private int health = 50;
+    [SerializeField] private ParticleSystem explosionFX;
 
     private void OnTriggerEnter2D(Collider2D collision) {
         DamageDealer damageDealer = collision.GetComponent<DamageDealer>();
         if(damageDealer != null) {
-            damageDealer.Hit();
             TakeDamage(damageDealer.GetDamageValue());
+            PlayExplosionFX();
+            damageDealer.Hit();
         }
     }
 
@@ -18,6 +20,13 @@ public class Health : MonoBehaviour
         health -= damageValue;
         if(health <= 0) {
             Destroy(gameObject);
+        }
+    }
+
+    private void PlayExplosionFX() {
+        if (explosionFX != null) {
+            ParticleSystem instance = Instantiate(explosionFX, transform.position, Quaternion.identity);
+            Destroy(instance.gameObject, instance.main.duration + instance.main.startLifetime.constantMax);
         }
     }
 }
