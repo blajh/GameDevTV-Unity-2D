9f54b9661ddcb13b6ed48e414168d51e0ed7b11d
Andrej Kovacevic <glazba@gmail.com>
2023-02-15 20:13:25 +0100

    Camera SloMo on Hit

--- GDTV - 2D - Laser Defender/Assets/Scripts/Health.cs
+++ GDTV - 2D - Laser Defender/Assets/Scripts/Health.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [Header("General")]
     [SerializeField] private int health = 50;
     [SerializeField] private ParticleSystem explosionFX;
 
+    [Header("Camera Shake")]
     [SerializeField] private bool applyCameraShake = false;
     CameraShake cameraShake;
+
+    [Header("Slow Motion")]
+    [SerializeField] private bool applySlowMotion = false;
+    [SerializeField] private float slowMotionDuration = 2f;
+    [SerializeField] private float slowMotionTimeScale = 0.5f;
+    private float sloMoTimer = 0f;
+    private bool gotHit = false;
 
     private void Awake() {
         cameraShake = Camera.main.GetComponent<CameraShake>();
@@ -21,6 +31,13 @@ public class Health : MonoBehaviour
             PlayExplosionFX();
             damageDealer.Hit();
             ShakeCamera();
+            gotHit = true;
+        }
+    }
+
+    private void Update() {
+        if (applySlowMotion && gotHit) {
+            SlowMotion();
         }
     }
 
@@ -41,6 +58,16 @@ public class Health : MonoBehaviour
     private void ShakeCamera() {
         if (cameraShake != null && applyCameraShake) {
             cameraShake.Play();
+        }
+    }
+
+    private void SlowMotion() {
+        Time.timeScale = slowMotionTimeScale + ((1 - slowMotionTimeScale) * (sloMoTimer / slowMotionDuration));
+        sloMoTimer += Time.deltaTime;
+        if (Time.timeScale >= 1) {
+            Time.timeScale = 1;
+            gotHit = false;
+            sloMoTimer = 0f;
         }
     }
 }
