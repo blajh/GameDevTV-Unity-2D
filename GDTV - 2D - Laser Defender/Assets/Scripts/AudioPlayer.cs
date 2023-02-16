3814c5a8efaf9e85f7e402c10294486089f3326a
Andrej Kovacevic <glazba@gmail.com>
2023-02-16 15:05:44 +0100

    Singleton Pattern

--- GDTV - 2D - Laser Defender/Assets/Scripts/AudioPlayer.cs
+++ GDTV - 2D - Laser Defender/Assets/Scripts/AudioPlayer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,6 +12,22 @@ public class AudioPlayer : MonoBehaviour
     [Header("Damage Taken")]
     [SerializeField] private AudioClip damageTakenClip;
     [SerializeField][Range(0f, 1f)] private float damageTakenVolume = 1f;
+
+    private static AudioPlayer Instance;
+
+    private void Awake() {
+        ManageSingleton();
+    }
+
+    private void ManageSingleton() {        
+        if (Instance != null) {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        } else {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
 
     public void PlayShootingClip() {
         PlayClip(shootingClip, shootingVolume);
